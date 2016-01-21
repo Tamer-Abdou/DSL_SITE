60142f6a5e6749172dc5133055c42910e940cd7c
Mefta Sadat <mefta.sadat@ryerson.ca>
2016-01-21 14:51:09 -0500

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'ex-std-node673.prod.rhcloud.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
